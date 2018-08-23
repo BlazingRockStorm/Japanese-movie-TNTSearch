1d80aa0eae153f9f21d8e2ffd0849b886ccc2d31
Hoang Duc Quan <rocker.gryphon@gmail.com>
2018-08-23 10:37:53 +0700

    add seeding

--- database/migrations/2018_08_22_171318_create_actresses_table.php
+++ database/migrations/2018_08_22_171318_create_actresses_table.php
@@ -16,6 +16,7 @@ class CreateActressesTable extends Migration
         Schema::create('actresses', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('picture');
             $table->timestamps();
         });
     }
---
+++ database/seeds/ActressesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ActressesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
