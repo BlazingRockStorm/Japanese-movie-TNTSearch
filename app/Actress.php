57de9b2d959d8d8c4a44b2ada54438ea36d5847d
Hoang Duc Quan <rocker.gryphon@gmail.com>
2018-08-23 10:41:38 +0700

    set timestamp false

--- app/Actress.php
+++ app/Actress.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Actress extends Model
 {
     protected $table='actresses';
+    public $timestamps = false;
 
     public function movies
     {
--- app/Movie.php
+++ app/Movie.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     protected $table='movies';
+    public $timestamps = false;
 
     public function actress
     {
