a0d14792c5d2096ead0fc3203ace9772e73e1764
Hoang Duc Quan <rocker.gryphon@gmail.com>
2018-08-23 13:18:01 +0700

    rewrite model

--- app/Movie.php
+++ app/Movie.php
@@ -3,11 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Movie extends Model
 {
+	use Searchable;
+
+    public $asYouType = true;//chỉ cần gõ 1 kí tự là cho ra các kết quả chứa kí tự ấy
     protected $table='movies';
     public $timestamps = false;
+
+	/**
+     * Get the indexable data array for the model.
+     *
+     * @return array
+     */
+    public function toSearchableArray()
+    {
+        $array = $this->toArray();
+
+        // Customize array...
+        $array['name'] = $this->name;
+        $array['actress'] = $this->actress['name'];
+
+        return $array;
+    }
 
     public function actress()
     {
--- config/app.php
+++ config/app.php
@@ -150,6 +150,8 @@ return [
         /*
          * Package Service Providers...
          */
+        TeamTNT\Scout\TNTSearchScoutServiceProvider::class,
+        Laravel\Scout\ScoutServiceProvider::class,
 
         /*
          * Application Service Providers...
